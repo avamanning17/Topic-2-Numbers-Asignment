3763889a64b7d91c2b9fa4ce1db6d83d696ff701
Ava Manning <362579849@GoTVDSB.ca>
2025-09-08 09:43:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Topic_2_Numbers_Asignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Ava Manning
+            Console.WriteLine("I was born in the year " + (2025 - 15));
+            Console.WriteLine("The average of the numbers 14,15,16 is" + (14.0 + 15 + 16) / 3.0);
+
+
+        }
+    }
+}

b0dcbee4d336e8ef28775b0fc198f72626827d9d
Ava Manning <362579849@GoTVDSB.ca>
2025-09-09 08:54:50 -0400

    Almost done

--- Program.cs
+++ Program.cs
@@ -8,8 +8,15 @@
 
             // Ava Manning
             Console.WriteLine("I was born in the year " + (2025 - 15));
+            Console.WriteLine();
             Console.WriteLine("The average of the numbers 14,15,16 is" + (14.0 + 15 + 16) / 3.0);
-
+            Console.WriteLine();
+            Console.WriteLine("The average of the numbers 14,15,16,17 is" + (14.0 + 15 + 16 + 17) / 4.0);
+            Console.WriteLine();
+            Console.WriteLine(" The area of a circle with the radius of 3 is" + (3.14159 * (3*3)));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
 
         }
     }
